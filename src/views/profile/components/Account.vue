eea38608f47b9da4a16a022e6ff197e41a1bf863
2639043230 <2639043230@qq.com>
2022-04-17 14:39:41 +0800

    4.17修改，个人主页修改

--- src/views/profile/components/Account.vue
+++ src/views/profile/components/Account.vue
@@ -1,92 +1,69 @@
 <template>
-  <el-form :model="form" label-width="80px">
+  <el-form label-width="80px">
     <el-form-item label="姓名">
-      <el-input v-model="username" class="input_" style="width: 40%;" />
+      <el-input v-model.trim="account.realName" style="width: 50%;" />
     </el-form-item>
-    <el-form-item label="手机">
-      <el-input v-model.trim="photo" style="width: 40%;" />
-    </el-form-item>
-    <el-form-item label="邮箱">
-      <el-input v-model.trim="email" style="width: 40%;" />
+    <el-form-item label="账号">
+      <el-input v-model.trim="account.userName" :disabled="true" style="width: 50%;" />
     </el-form-item>
     <el-form-item label="学校" prop="name">
-      <el-input v-model.trim="school" style="width: 40%;" />
+      <el-input v-model.trim="account.userUniv" style="width: 50%;" />
     </el-form-item>
-    <el-form-item label="年级">
-      <el-input v-model.trim="grade" style="width: 40%;" />
+    <el-form-item label="专业">
+      <el-input v-model.trim="account.userUnit" style="width: 50%;" />
     </el-form-item>
-    <el-form-item label="年龄" prop="age">
-      <el-input v-model.trim="age" style="width: 40%;" />
-    </el-form-item>
-    <el-form-item label="性别" prop="gender">
-      <el-select v-model="gender" placeholder="请选择" style="width: 40%;">
-        <el-option :value="0" label="男" />
-        <el-option :value="1" label="女" />
-      </el-select>
-    </el-form-item>
-    <el-form-item label="出生日期">
-      <el-input v-model.trim="birthday" style="width: 40%;" />
-    </el-form-item>
+<!--    <el-form-item label="性别" prop="gender">-->
+<!--      <el-select v-model="gender" placeholder="请选择" style="width: 40%;">-->
+<!--        <el-option :value="0" label="男" />-->
+<!--        <el-option :value="1" label="女" />-->
+<!--      </el-select>-->
+<!--    </el-form-item>-->
+<!--    <el-form-item label="出生日期">-->
+<!--      <el-input v-model.trim="birthday" style="width: 40%;" />-->
+<!--    </el-form-item>-->
     <el-form-item>
       <el-button type="primary" icon="el-icon-edit" @click="submit">修改</el-button>
-      <el-button type="primary" @click="submit">确定</el-button>
-      <el-button type="primary" @click="submit">取消</el-button>
+<!--      <el-button type="primary" @click="submit">提交</el-button>-->
+<!--      <el-button type="primary" @click="submit">取消</el-button>-->
     </el-form-item>
   </el-form>
 
 </template>
 
 <script>
+import request from '@/utils/request'
+
 export default {
-  props: {
-    user: {
-      type: Object,
-      default: () => {
-        return {
-          username:'',
-          account: 'aa',
-          email: 'aa',
-          size: 'aa',
-          gender: 'aa',
-          age: '',
-          grade: '',
-          school: '',
-          photo: ''
-        }
-      }
-    }
-  },
+  props:['account'],
   data(){
     return {
-      username:'王信',
-      account: '19942421321',
-      email: '1300913599@upc.com',
-      size: '',
-      gender: '男',
-      age: '22',
-      grade: '3',
-      school: '中国石油大学华东',
-      photo: '12431242',
-      birthday:'7.9',
+      account: null
     }
   },
   methods: {
     submit() {
+      this.submitData()
       this.$message({
-        message: 'User information has been updated successfully',
+        message: '用户信息已更新',
         type: 'success',
         duration: 5 * 1000
       })
+    },
+    submitData(num) {
+      request({
+        url: 'auth/user/update',
+        method: 'put',
+        params: this.account
+      }).then(response => {
+        console.log(response)
+      }).catch(err => {
+        console.log(err)
+      })
     }
-    // display() {
-    //   this.
-    // }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  .input_ >>> .el-input__inner{
-    border: 0;
-  }
+
 </style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="app-container">
-    <div v-if="user">
+    <div>
       <el-row :gutter="20">
 
         <el-col :span="6" :xs="24">
-          <user-card :user="user" />
+          <user-card v-if="flag" :account="account" />
         </el-col>
 
         <el-col :span="18" :xs="24">
           <el-card>
             <el-tabs v-model="activeTab">
               <el-tab-pane label="个人信息" name="account">
-                <account :user="user" />
+                <account v-if="flag" :account="account"/>
               </el-tab-pane>
-              <el-tab-pane label="消息通知" name="activity">
-                <activity />
+              <el-tab-pane v-if="account.roleId===1" label="消息通知" name="activity">
+                <activity :account="account" />
               </el-tab-pane>
               <el-tab-pane label="时间线" name="timeline">
                 <timeline />
@@ -29,46 +29,39 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import UserCard from './components/UserCard'
 import Activity from './components/Activity'
 import Timeline from './components/Timeline'
 import Account from './components/Account'
+import request from '@/utils/request'
 
 export default {
   components: { UserCard, Activity, Timeline, Account },
   data() {
     return {
-      user: {},
+      account: null,
+      flag: false,
       activeTab: 'account'
     }
   },
-  computed: {
-    ...mapGetters([
-      'name',
-      'avatar',
-      'roles',
-      'size'
-    ])
-  },
   created() {
-    this.getUser()
+    this.fetchAccountData()
   },
   methods: {
-    getUser() {
-      this.user = {
-        name: this.name,
-        account: 13001660207,
-        role: this.roles.join(' | '),
-        email: 'admin@test.com',
-        avatar: this.avatar,
-        size: '10',
-        gender: '男',
-        age: '21',
-        grade: '大三',
-        school: '中国石油大学（华东）',
-        photo: '13001660207'
-      }
+    fetchAccountData() {
+      request({
+        url: 'auth/client/get',
+        method: 'get',
+        params: {
+          userName: this.$store.getters.name
+        }
+      }).then(response => {
+        console.log(response)
+        this.flag = true
+        this.account = response
+      }).catch(err => {
+        console.log(err)
+      })
     }
   }
 }
